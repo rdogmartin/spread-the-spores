a7c636f0f9ce4d985e38981e06e9db94b7f32ba1
Roger Martin <RogerM@tasconline.com>
2020-12-12 15:43:36 -0700

    Simple 3x3 grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'spread-the-spores';
+  public cell1CssClass = 'empty';
+
+  public cell1Clicked() {
+    this.cell1CssClass = 'player1';
+  }
 }
